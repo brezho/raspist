04d6c309564bd1e5871b002d3fb351f1bae3a86c
brezho <glebars@cargochain.com>
2020-07-01 21:25:37 +0200

    Update onboot.sh

--- onboot.sh
+++ onboot.sh
@@ -9,10 +9,10 @@ if [[ ! -d /home/pi/version ]]; then
 fi
 
 if [[ ! -e /home/pi/version/001 ]]; then
-  echo "apt" >> $logfile;
+  echo "apt" > $logfile;
   sudo apt-get update;
   sudo apt-get upgrade;
-  echo "download dotnet" >> $logfile;
+  echo "download dotnet" > $logfile;
   wget https://download.visualstudio.microsoft.com/download/pr/8ccacf09-e5eb-481b-a407-2398b08ac6ac/1cef921566cb9d1ca8c742c9c26a521c/aspnetcore-runtime-3.1.2-linux-arm.tar.gz;
   mkdir /home/pi/dotnet-arm32;
   tar zxf aspnetcore-runtime-3.1.2-linux-arm.tar.gz -C /home/pi/dotnet-arm32
@@ -21,4 +21,4 @@ fi
 
 export DOTNET_ROOT=/home/pi/dotnet-arm32;
 export PATH=$PATH:/home/pi/dotnet-arm32;
-echo "path = $PATH" >> $logfile;
+echo "path = $PATH" > $logfile;
